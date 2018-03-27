36cc694fa51fd9e1901fb9dfeae313be81396bcb
pesiik <pesiik@mail.ru>
2018-03-27 14:03:57 +0400

    Orientation learn. Step 2.

--- OrientationLearn/app/src/main/java/com/example/user/orientationlearn/MainActivity.java
+++ OrientationLearn/app/src/main/java/com/example/user/orientationlearn/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.user.orientationlearn;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+    static final String ORIENTATION_PORTRAIT = "Портретный режим";
+    static final String ORIENTATION_LANDSCAPE = "Альбомный режим";
+
+    // определяем изменение ориентации экрана
+    boolean mState = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText)findViewById(R.id.editTest);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Toast.makeText(MainActivity.this,
+                        "onTextChanged: " + s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
+
 }
